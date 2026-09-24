a84ae0832b1964e4d5c4833103d23b755debf765
LEI-99917 <ampco2@iscte-iul.pt>
2026-09-24 15:21:25 +0100

    Fix Maven configuration and update JUnit tests

--- src/test/java/iscteiul/ista/AppTest.java
+++ src/test/java/iscteiul/ista/AppTest.java
@@ -1,38 +1,23 @@
+
 package iscteiul.ista;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * Unit test for simple App.
+ * Testes unitários básicos da aplicação Battleship.
+ *
+ * @author Afonso Carolo
  */
-public class AppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
     /**
-     * @return the suite of tests being tested
+     * Verifica a execução de um teste simples.
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        assertTrue(true);
     }
 }
+  
